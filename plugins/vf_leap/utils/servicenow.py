c2d0df6a26f05556b94ca4e8172a3ab76b017cd7
Shahbaz Ali <shahbaz.ali03@gmail.com>
2020-02-15 23:46:00 +0530

    adds utils package to the plugin

---
+++ plugins/vf_leap/utils/servicenow.py
@@ -0,0 +1,8 @@
+#   vf_leap
+#   Copyright (c)Cloud Innovation Partners 2020.
+#   Author : Shahbaz Ali
+
+
+def fetch_servicenow_record_count():
+    # TODO implement logic for fetching record count
+    pass
