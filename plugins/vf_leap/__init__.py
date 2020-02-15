157dbd971c8ab00794cd4e836857abbb2c92500a
Shahbaz Ali <shahbaz.ali03@gmail.com>
2020-02-15 23:48:18 +0530

    adds operator entry to the plugin

--- plugins/vf_leap/__init__.py
+++ plugins/vf_leap/__init__.py
@@ -1,10 +1,16 @@
+
+#   vf_leap
+#   Copyright (c)Cloud Innovation Partners 2020.
+#   Author : Shahbaz Ali
+
 from airflow.plugins_manager import AirflowPlugin
 
-from plugins.vf_leap.operators.servicenow_to_s3_transfer_operator import ServiceNow2S3TransferOperator
+from plugins.vf_leap.operators.servicenow_to_s3_transfer_operator import ServiceNowToS3TransferOperator
+from plugins.vf_leap.operators.servicenow_to_sftp_transfer_operator import ServiceNowToSFTPTransferOperator
 class VFLEAP(AirflowPlugin):
     name = 'VFLEAP'
     hooks = []
-    operators = [ServiceNow2S3TransferOperator]
+    operators = [ServiceNowToS3TransferOperator,ServiceNowToSFTPTransferOperator]
     executors = []
     macros = []
     menu_links = []
