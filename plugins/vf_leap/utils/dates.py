152c7348bfe65e11a9992095a2d956942178e5bb
Shahbaz Ali <shahbaz.ali03@gmail.com>
2020-02-24 23:55:51 +0530

    adds half-hourly cron expression to the jinja template

---
+++ plugins/vf_leap/utils/dates.py
@@ -0,0 +1,8 @@
+#   vf_leap
+#   Copyright (c)Cloud Innovation Partners 2020.
+#   Author : Shahbaz Ali
+
+
+from airflow.utils.dates import cron_presets
+
+cron_presets['@half-hourly'] ='*/30 * * * *'
