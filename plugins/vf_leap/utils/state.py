6e67601a43e9da22d3cf15d4780d5dfb4b5c5534
Shahbaz Ali <shahbaz.ali03@gmail.com>
2020-02-29 22:52:50 +0530

    adds a state.py file as a way of injecting changes to the airflow states

---
+++ plugins/vf_leap/utils/state.py
@@ -0,0 +1,16 @@
+#   vf_leap
+#   Copyright (c)Cloud Innovation Partners 2020.
+#   Author : Shahbaz Ali
+
+
+'''
+This is a modified implementation of airflow State class
+'''
+
+from airflow.utils.state import State
+
+RECOVERY_EXECUTED = "recovery_executed"
+State.RECOVERY_EXECUTED = "recovery_executed"
+State.task_states + (RECOVERY_EXECUTED,)
+State.dag_states + (RECOVERY_EXECUTED,)
+State.state_color[RECOVERY_EXECUTED] = 'magenta'
