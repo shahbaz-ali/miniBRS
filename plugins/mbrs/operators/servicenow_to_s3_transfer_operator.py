480fe9fa9f546af76e95644a8f8358ff7d53d373
mdakeel <mdakeel.adeel@gmail.com>
2020-03-31 18:25:42 +0530

    Adds region-name and bucket-name to Extras of s3_default and resolves the issue regarding load_file() method

--- plugins/mbrs/operators/servicenow_to_s3_transfer_operator.py
+++ plugins/mbrs/operators/servicenow_to_s3_transfer_operator.py
@@ -11,6 +11,7 @@ from airflow import configuration
 from datetime import datetime, timedelta
 from plugins.mbrs.hooks.amazon_s3_hook import S3HookWrapper
 from botocore.exceptions import ClientError
+import json
 
 class ServiceNowToS3TransferOperator(ServiceNowToGenericTransferOperator):
 
@@ -23,8 +24,9 @@ class ServiceNowToS3TransferOperator(ServiceNowToGenericTransferOperator):
             raise S3ConnectionNotFoundException()
 
         dt_current = datetime.strptime(self.execution_date[:19], "%Y-%m-%dT%H:%M:%S")
-        l_file_path = self.file_name.replace('.csv', '.json')
-        file_name = l_file_path[l_file_path.rfind('/') + 1:]
+        self.l_file_path = self.file_name
+        index = self.l_file_path.rfind('/')
+        file_name = self.l_file_path[index + 1:]
 
         exec_hour = str(dt_current.hour)
         exec_minute = str(dt_current.minute)
@@ -56,21 +58,22 @@ class ServiceNowToS3TransferOperator(ServiceNowToGenericTransferOperator):
 
         try:
             s3_hook = S3HookWrapper(
-                aws_secret_key_id=credentials_s3.login,
-                aws_access_key_id=credentials_s3.password
+                aws_secret_key_id=credentials_s3.password,
+                aws_access_key_id=credentials_s3.login
             )
 
             if s3_hook._check_aws_credentails():
-                if not s3_hook.check_for_bucket(bucket_name='vf-leap'):
-                    s3_hook.create_bucket(bucket_name='vf-leap', region_name=self.region_name)
+                bucket_name = json.loads(credentials_s3.get_extra())["bucket-name"]
+                if not s3_hook.check_for_bucket(bucket_name=bucket_name):
+                    region_name = json.loads(credentials_s3.get_extra())["region-name"]
+                    s3_hook.create_bucket(bucket_name=bucket_name, region_name=region_name)
 
             else:
                 LoggingMixin().log.warning("Authentication Error : invalid s3 credentials")
-
-            s3_hook.load_file(key=r_file_path, filename=self.l_file_path, bucket_name='vf-leap', replace=True)
+            s3_hook.load_file(filename=self.l_file_path, key=r_file_path, bucket_name=bucket_name, replace=True)
 
         except (Exception, ClientError) as e:
             if isinstance(e, ClientError) and e.response['Error']['Code'] == 'BucketAlreadyOwnedByYou':
                 return True
-            else:
-                LoggingMixin().log.error("error in AmazonS3 connection")
+
+            raise
