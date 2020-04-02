4c6416d0b437a47cedfcfff812f5320eaf6c5834
mdakeel <mdakeel.adeel@gmail.com>
2020-04-02 10:51:00 +0530

    Adds test cases for ServiceNow Client

--- plugins/mbrs/utils/test/test_servicenow_client.py
+++ plugins/mbrs/utils/test/test_servicenow_client.py
@@ -14,19 +14,14 @@ class TestServiceClientNow(unittest.TestCase):
         'refresh_token': None
     }
 
-    # def setUp(self):
-    #     self.service_now_client=ServiceNowClient()
-    #
-    # def tearDown(self):
-    #     self.service_now_client=None
-
+    #OK
     def test_object_creation_case1(self):
         # without  parameters
 
        with self.assertRaises(InvalidArguments):
            ServiceNowClient()
 
-
+    #OK
     def test_object_creation_case2(self):
 
         self.assertRaises(InvalidArguments, ServiceNowClient,
@@ -34,6 +29,7 @@ class TestServiceClientNow(unittest.TestCase):
                           password=self.sample_args.get('password'),
                           )
 
+    #OK
     def test_object_creation_case3(self):
 
         self.assertRaises(InvalidArguments, ServiceNowClient,
@@ -41,6 +37,7 @@ class TestServiceClientNow(unittest.TestCase):
                           password=self.sample_args.get('password'),
                           )
 
+    #OK
     def test_object_creation_case4(self):
 
         self.assertRaises(InvalidArguments, ServiceNowClient,
@@ -48,6 +45,7 @@ class TestServiceClientNow(unittest.TestCase):
                           login=self.sample_args.get('login'),
                           )
 
+    #OK
     def test_object_creation_case5(self):
 
         self.assertRaises(InvalidArguments, ServiceNowClient,
@@ -56,6 +54,7 @@ class TestServiceClientNow(unittest.TestCase):
                 password=self.sample_args.get('password')
             )
 
+    #OK
     def test_object_creation_case6(self):
         self.assertRaises(InvalidArguments, ServiceNowClient,
                           host="",
@@ -63,27 +62,14 @@ class TestServiceClientNow(unittest.TestCase):
                           password=""
                           )
 
-    # def test_object_creation_case6(self):
-    #
-    #     try:
-    #         ServiceNowClient(,,
-    #             host=self.default_args.get('host'),
-    #             login=self.default_args.get('login'),
-    #             password=self.default_args.get('password'),
-    #             auth_type=1,
-    #             client_id=self.default_args.get('client_id'),
-    #             client_secret=self.default_args.get('client_secret'),
-    #             token=self.default_args.get('token'),
-    #             refresh_token=self.default_args.get('refresh_token')
-    #         )
-    #     except ServiceNowException:
-    #         self.fail("System Failure : ServiceNowClient Raised Exception")
+    #OK
+    def test_object_creation_case7(self):
+        self.assertRaises(InvalidArguments, ServiceNowClient,
+                          host="",
+                          login="",
+                          password="*******"
+                          )
 
-
-    # def test_fetchAccessToken(self):
-    #
-    #     with self.assertRaises(InvalidArguments):
-    #         self.service_now_client.fet
 
 if __name__=='__main__':
     unittest.main()
