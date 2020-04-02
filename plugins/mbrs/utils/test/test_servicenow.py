7eef2587ac317ec21a224c2d689696dee3e5e025
mdakeel <mdakeel.adeel@gmail.com>
2020-04-02 10:49:44 +0530

    Adds test cases for ServiceNow

--- plugins/mbrs/utils/test/test_servicenow.py
+++ plugins/mbrs/utils/test/test_servicenow.py
@@ -5,6 +5,7 @@ from plugins.mbrs.utils.exceptions import InvalidArguments, ConfigVariableNotFou
 
 class TestServiceNow(unittest.TestCase):
 
+    #OK
     def test_fetch_servicenow_record_count(self):
 
         with self.assertRaises(InvalidArguments):
@@ -17,43 +18,29 @@ class TestServiceNow(unittest.TestCase):
         with self.assertRaises(InvalidArguments):
             fetch_servicenow_record_count(None, None)
 
-        with self.assertRaises(KeyError):
-            fetch_servicenow_record_count("incident", datetime.now())
+    #OK
+    def test_on_failure_email(self):
 
-        #keep the below cases separate, it has to be implements in integration phase
-        # with self.assertRaises(ConfigVariableNotFoundException):
-        #     fetch_servicenow_record_count("incident", datetime.now())
+      with self.assertRaises(InvalidArguments):
+          on_failure_email("", "", "")
 
-        # with self.assertRaises(ServiceNowConnectionNotFoundException):
-        #     fetch_servicenow_record_count("incident", datetime.now())
-        #
-        # self.assertIsInstance(fetch_servicenow_record_count("incident", datetime.now()), str, msg=None)
+      with self.assertRaises(InvalidArguments):
+           on_failure_email("incident", "any_tag_id","")
 
+    #OK
+    def test_on_failure_context(self):
 
-    #
-    #def test_on_failure_email(self):
-    #
-    #   with self.assertRaises(InvalidArguments):
-    #       on_failure_email("", "", "")
+        with self.assertRaises(InvalidArguments):
+            on_failure_context("", "", "", "", "")
 
+        with self.assertRaises(InvalidArguments):
+            on_failure_context("incident", "", "", "", "")
 
-    #    with self.assertRaises(InvalidArguments):
-    #        on_failure_email("incident", "any_tag_id","")
-    #
-    #
-    # def test_on_failure_context(self):
-    #
-    #     with self.assertRaises(InvalidArguments):
-    #         on_failure_context("", "", "", "", "")
-    #
-    #     with self.assertRaises(InvalidArguments):
-    #         on_failure_context("incident", "", "", "", "")
-    #
-    #
-    # def test_clean_up(self):
-    #
-    #     with self.assertRaises(InvalidArguments):
-    #         clean_up("incident", "", "")
+    #OK
+    def test_clean_up(self):
+
+        with self.assertRaises(InvalidArguments):
+            clean_up("incident", "", "")
 
 if __name__ == '__main__':
     unittest.main()
