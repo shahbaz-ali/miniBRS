6d277076dc2780dc9a919d15ec43224d4baebdc6
Shahbaz Ali <shahbaz.ali03@gmail.com>
2020-04-16 15:48:57 +0530

    Updates recovey_dashboard_view.py
    * Refactor code as per PEP8 standards

--- plugins/mbrs/views/recovery_dashboard_view.py
+++ plugins/mbrs/views/recovery_dashboard_view.py
@@ -1,13 +1,23 @@
 #   mbrs
 #   Copyright (c)Cloud Innovation Partners 2020.
-#   Author : Shahbaz Ali
+#   http://cloudinp.com
 
+"""
+This module contains ModelView class, and add-on functions for view customization
+These ModelView class are used by airflow plugin manager to render the custom mini BRS
+views
+
+    Example :
+        RecoveryDashboard maps to FailedDags in airflow UI
+
+"""
+import json
+import datetime as dt
 import airflow
 from flask_admin import expose
-from flask import Markup
+from flask import Markup, request
 from flask_admin.contrib.sqla import ModelView
 from flask_admin.actions import action
-from plugins.mbrs.modals.recovery_modals import FailedDagRun, Reason
 from wtforms import validators
 from airflow.utils import timezone
 from airflow.utils.state import State
@@ -15,37 +25,75 @@ from airflow.utils.db import provide_session
 from airflow.utils.log.logging_mixin import LoggingMixin
 from airflow.models.variable import Variable
 from sqlalchemy import func
-import json
-from flask import request
-import datetime as dt
+from plugins.mbrs.modals.recovery_modals import FailedDagRun, Reason
 
 airflow.load_login()
 
 current_user = airflow.login.current_user
 
+
 def parse_datetime_f(value):
+    """
+    Flask Admin Helper function
+    :param value:
+    :return:
+    """
     if not isinstance(value, dt.datetime):
         return value
 
     return timezone.make_aware(value)
 
-def datetime_f(v, c, m, p):
-    attr = getattr(m, p)
-    dttm = attr.isoformat() if attr else ''
-    if timezone.utcnow().isoformat()[:4] == dttm[:4]:
-        dttm = dttm[5:]
-    return Markup("<nobr>{}</nobr>").format(dttm)
 
-def state_f(v, c, m, p):
-    state = m.state
-    color = State.color(m.state)
+def datetime_f(view, context, model, name):
+    """
+    Flask Admin Helper function
+    Purpose : used to parse the `execution_date`
+    :param view:
+    :param context:
+    :param model:
+    :param name:
+    :return:
+    """
+
+    attr = getattr(model, name)
+    datetime = attr.isoformat() if attr else ''
+    if timezone.utcnow().isoformat()[:4] == datetime[:4]:
+        datetime = datetime[5:]
+    return Markup("<nobr>{}</nobr>").format(datetime)
+
+
+def state_f(view, context, model, name):
+    """
+    Flask Admin Helper function
+    Purpose : used to set the state color
+    :param view:
+    :param context:
+    :param model:
+    :param name:
+    :return:
+    """
+    state = model.state
+    color = State.color(model.state)
     return Markup(
         '<span class="label" style="background-color:{color};">'
         '{state}</span>').format(**locals())
 
-def reason_f(v, c, m, p):
-    markupstring = "<a href='http://{}/admin/task_fail_reason/{}${}'>view details</a>".format(request.host,str(m.execution_date)[:19],m.dag_id)
+
+def reason_f(view, context, model, name):
+    """
+    Flask Admin Helper function
+    Purpose : used to show `view_details` in the Flask Admin list_view
+    :param view:
+    :param context:
+    :param model:
+    :param name:
+    :return:
+    """
+
+    markupstring = "<a href='http://{}/admin/task_fail_reason/{}${}'>view details</a>"\
+        .format(request.host, str(model.execution_date)[:19], model.dag_id)
     return Markup(markupstring)
+
 
 class RecoveryDashboard(ModelView):
 
@@ -64,7 +112,7 @@ class RecoveryDashboard(ModelView):
             ('success', 'success'),
             ('running', 'running'),
             ('failed', 'failed'),
-            ('recovery_executed','recovery_executed'),
+            ('recovery_executed', 'recovery_executed'),
         ],
     }
     form_args = {
@@ -80,22 +128,30 @@ class RecoveryDashboard(ModelView):
         }
     }
 
-
     column_list = (
         'state', 'dag_id', 'execution_date', 'run_id', 'external_trigger', 'failure_reason')
-    column_filters = ('state','dag_id')
+    column_filters = ('state', 'dag_id')
     column_searchable_list = ('dag_id', 'state', 'run_id')
 
     column_formatters = dict(
         execution_date=datetime_f,
         state=state_f,
-        failure_reason = reason_f
+        failure_reason=reason_f
     )
 
-    @action('trigger_dag','Trigger DAG','Are you sure you want to re-run this dag, you can run this dag only once, make sure all dependencies are met')
+    @action('trigger_dag',
+            'Trigger DAG',
+            'Are you sure you want to re-run this dag, you can run this dag only once, '
+            'make sure all dependencies are met')
     @provide_session
-
-    def trigger_dag(self,ids,session=None):
+    def trigger_dag(self, ids, session=None):
+        """
+        Flask Admin `action` method
+        Purpose : used to trigger the selected Failed DAG
+        :param ids:
+        :param session:
+        :return:
+        """
 
         rows = session.query(FailedDagRun).filter(FailedDagRun.id.in_(ids)).all()
 
@@ -103,53 +159,76 @@ class RecoveryDashboard(ModelView):
             r_config = Variable.get(key='r_config')
             r_obj = json.loads(r_config)
 
-            for d in rows:
-                if r_obj.__contains__(d.dag_id):
-                    if not (r_obj[d.dag_id]).__contains__(str(d.execution_date)[:19]):
-                        r_obj[d.dag_id].append(str(d.execution_date)[:19])
+            for id_ in rows:
+                if r_obj.__contains__(id_.dag_id):
+                    if not (r_obj[id_.dag_id]).__contains__(str(id_.execution_date)[:19]):
+                        r_obj[id_.dag_id].append(str(id_.execution_date)[:19])
                     else:
                         pass
                 else:
-                    r_obj[d.dag_id] = [str(d.execution_date)[:19]]
+                    r_obj[id_.dag_id] = [str(id_.execution_date)[:19]]
 
-            Variable.set(key='r_config',value=json.dumps(r_obj))
+            Variable.set(key='r_config', value=json.dumps(r_obj))
 
-            for id in ids:
-                execution_date = session.query(FailedDagRun).filter(FailedDagRun.id==id).one().execution_date
-                dag_id = session.query(FailedDagRun).filter(FailedDagRun.id==id).one().dag_id
+            for id_ in ids:
 
-                session.query(FailedDagRun).filter(FailedDagRun.id == id).update({'state': 'recovery_executed'}, synchronize_session='fetch')
-                Variable.delete(key="{}${}".format(str(execution_date)[:19],dag_id))
+                execution_date = session.query(FailedDagRun)\
+                    .filter(FailedDagRun.id == id_)\
+                    .one()\
+                    .execution_date
+
+                dag_id = session.query(FailedDagRun)\
+                    .filter(FailedDagRun.id == id_)\
+                    .one()\
+                    .dag_id
+
+                session.query(FailedDagRun).filter(FailedDagRun.id == id_).update(
+                    {'state': 'recovery_executed'}, synchronize_session='fetch')
+
+                Variable.delete(key="{}${}".format(str(execution_date)[:19], dag_id))
 
         except KeyError as e:
             LoggingMixin().log.warn(e.__str__())
-            Variable.set(key='r_config',value='{}')
-            self.create_r_config(ids,session)
+            Variable.set(key='r_config', value='{}')
+            RecoveryDashboard.create_r_config(ids, session)
 
-
-
-    def create_r_config(self,ids,session):
+    @staticmethod
+    def create_r_config(ids, session):
+        """
+        This method is used to create and populate `r_config` variable  
+        :param ids:
+        :param session:
+        :return:
+        """
 
         rows = session.query(FailedDagRun).filter(FailedDagRun.id.in_(ids)).all()
 
         r_obj = {}
 
-        for d in rows:
-            if r_obj.__contains__(d.dag_id):
-                if not (r_obj[d.dag_id]).__contains__(d.execution_date):
-                    r_obj[d.dag_id].append(str(d.execution_date))
+        for row in rows:
+            if r_obj.__contains__(row.dag_id):
+                if not (r_obj[row.dag_id]).__contains__(row.execution_date):
+                    r_obj[row.dag_id].append(str(row.execution_date))
             else:
-                r_obj[d.dag_id] = [str(d.execution_date)[:19]]
+                r_obj[row.dag_id] = [str(row.execution_date)[:19]]
 
         Variable.set(key='r_config', value=json.dumps(r_obj))
 
-        for id in ids:
-            execution_date = session.query(FailedDagRun).filter(FailedDagRun.id == id).one().execution_date
-            dag_id = session.query(FailedDagRun).filter(FailedDagRun.id == id).one().dag_id
+        for id_ in ids:
+            execution_date = session.query(FailedDagRun)\
+                .filter(FailedDagRun.id == id_)\
+                .one()\
+                .execution_date
 
-            session.query(FailedDagRun).filter(FailedDagRun.id == id).update({'state': 'recovery_executed'},
-                                                                             synchronize_session='fetch')
-            Variable.delete(key="{}${}".format(str(execution_date)[:19],dag_id))
+            dag_id = session.query(FailedDagRun)\
+                .filter(FailedDagRun.id == id_)\
+                .one()\
+                .dag_id
+
+            session.query(FailedDagRun).filter(FailedDagRun.id == id_).update(
+                {'state': 'recovery_executed'}, synchronize_session='fetch')
+
+            Variable.delete(key="{}${}".format(str(execution_date)[:19], dag_id))
 
     def is_accessible(self):
         return current_user.is_authenticated
@@ -159,13 +238,14 @@ class RecoveryDashboard(ModelView):
             Default filters for model
             """
 
-        return super(RecoveryDashboard, self).get_query().filter(FailedDagRun.get_state(FailedDagRun) == 'failed' )
-
-
+        return super(RecoveryDashboard, self).get_query()\
+            .filter(FailedDagRun.get_state(FailedDagRun) == 'failed')
 
     def get_count_query(self):
 
-        self.session.query(func.count('*')).select_from(FailedDagRun).filter(FailedDagRun.get_state(FailedDagRun) == 'failed')
+        self.session.query(func.count('*'))\
+            .select_from(FailedDagRun)\
+            .filter(FailedDagRun.get_state(FailedDagRun) == 'failed')
 
 
 class TaskInstanceFailureVariable(ModelView):
@@ -180,21 +260,17 @@ class TaskInstanceFailureVariable(ModelView):
     column_list = (
         'key', 'val')
     column_labels = {
-        'val' : 'Value'
+        'val': 'Value'
     }
-
 
     def is_visible(self):
         return False
 
-    @expose('/<string:id>',methods=['GET'])
-    def __index__(self,id):
-        self.param_id = id
+    @expose('/<string:id_>', methods=['GET'])
+    def __index__(self, id_):
+        self.param_id = id_
         return super().index_view()
 
     def get_query(self):
-        x = super(TaskInstanceFailureVariable, self).get_query().filter(Reason.key == str(self.param_id))
-        print(x)
-        return x
-
-
+        return super(TaskInstanceFailureVariable, self).get_query()\
+            .filter(Reason.key == str(self.param_id))
