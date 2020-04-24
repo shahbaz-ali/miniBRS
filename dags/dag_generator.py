adbda8cbb5d790aa6865b5a5d6de387dc624764e
Shahbaz Ali <shahbaz.ali03@gmail.com>
2020-04-24 14:38:41 +0530

    Fixes issue #14 : Hide dag_generator from UI
    - removes DAG instantiation from dag_generator file

--- dags/dag_generator.py
+++ dags/dag_generator.py
@@ -3,30 +3,19 @@
 #   http://www.cloudinp.com
 
 """
-This module represents an Airflow DAG, responsible for
+This is an abstract DAG, responsible for
 generating user DAGs dynamically using a `config` variable
 from the Airflow meta-database
 """
 
-from datetime import datetime
-from airflow import DAG
+from airflow import DAG  # required
 from plugins.mbrs.utils import generator
 
-with DAG(
-        dag_id='dag_generator',
-        description='Generates DAG corresponding to a specific table',
-        schedule_interval=None,
-        start_date=datetime(2020, 11, 1),
-        catchup=False,
-        default_args={
-            'owner': 'BRS',
-        }
-) as dag:
 
-    if not generator.ini():
-        generator.is_servicenow_default_connection_available()
-        generator.is_rest_connection_available()
-        generator.is_config_variable_set()
-        generator.is_storage_defined()
-        generator.is_recovery_variable_set()
-        generator.create_dags()
+if not generator.ini():
+    generator.is_servicenow_default_connection_available()
+    generator.is_rest_connection_available()
+    generator.is_config_variable_set()
+    generator.is_storage_defined()
+    generator.is_recovery_variable_set()
+    generator.create_dags()
